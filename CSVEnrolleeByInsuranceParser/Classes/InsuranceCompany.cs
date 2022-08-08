4422de3bb74af8ce7c20c1553c7c41c384218d54
Destry Diefenbach <desdief@hotmail.com>
2022-08-07 21:34:14 -0500

    Add project files.

---
+++ CSVEnrolleeByInsuranceParser/Classes/InsuranceCompany.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVEnrolleeByInsuranceParser
+{
+    public class InsuranceCompany
+    {
+        private readonly string insurerName;
+        public readonly Dictionary<string,Enrollee> enrollees = new();
+
+        public InsuranceCompany(string name)
+        {
+            insurerName = name;
+        }
+
+        public void UpdateEnrollee(string[] enrolleeInfo)
+        {
+            //If the enrollee is not in the dictionary, make a new entry and a new Enrollee object
+            if (!enrollees.ContainsKey(enrolleeInfo[(int)EnrolleeData.UserID]))
+                enrollees[enrolleeInfo[(int)EnrolleeData.UserID]] = new Enrollee(enrolleeInfo);
+            
+            int newVersion = 0;
+            Int32.TryParse(enrolleeInfo[(int)EnrolleeData.Version], out newVersion);
+
+            //Check version of the enrollee entry with the new data 
+            enrollees[enrolleeInfo[(int)EnrolleeData.UserID]].CheckVersion(newVersion);
+        }
+
+        
+    }
+
+    public class Enrollee
+    {
+        private readonly string userId;
+        public string firstName;
+        public string lastName;
+        public int version = 0;
+        public string insurer;
+
+        public Enrollee(string[] values)
+        {
+            userId = values[(int)EnrolleeData.UserID];
+            firstName = values[(int)EnrolleeData.FirstName];
+            lastName = values[(int)EnrolleeData.LastName];
+            Int32.TryParse(values[(int)EnrolleeData.Version], out version);
+            insurer = values[(int)EnrolleeData.Insurer];
+        }
+
+        public void CheckVersion(int newVersion)
+        {
+            //If the new version is higher, update the version number
+            if (version < newVersion)
+                version = newVersion;
+        }
+        public override string ToString()
+        {
+            //Return the string in a csv format
+            return ($"{userId},{firstName},{lastName},{version},{insurer}");
+        }
+    }
+    //This enum corrosponds to the order of the csv data in the file.
+    public enum EnrolleeData
+    {
+        UserID = 0,
+        FirstName = 1,
+        LastName = 2,
+        Version = 3,
+        Insurer = 4
+    }
+}
---
+++ CSVEnrolleeByInsuranceParser/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using CSVEnrolleeByInsuranceParser;
+using System.Text;
+
+string sampleCSV = @"sample.csv";
+string sortedCSV = @"results.csv";
+
+//Split the lines of the CSV file into an array of strings
+string[] dataLines = File.ReadAllText(sampleCSV).Split('\n');
+
+//Sort the lines to put the Enrollees into the corrosponding Insurance Companies
+foreach (string line in dataLines.Skip(1)) //Skip the line with the column names.
+    SortLine(line.Trim());
+
+//Display the results and save them to a new csv file.
+DisplayResults();
+
+void SortLine(string line)
+{
+    //Split apart the line into it's individual values.
+    string[] enrolleeInfo = line.Split(',');
+
+    //If the Insurer isn't found in the dictionary make a new entry and create a new InsuranceCompany object
+    if (!GlobalVars.insuranceCompanies.ContainsKey(enrolleeInfo[(int)EnrolleeData.Insurer]))
+        GlobalVars.insuranceCompanies[enrolleeInfo[(int)EnrolleeData.Insurer]] = new InsuranceCompany(enrolleeInfo[(int)EnrolleeData.Insurer]);
+
+    //Update the InsuranceCompany object with the new enrollee data
+    GlobalVars.insuranceCompanies[enrolleeInfo[(int)EnrolleeData.Insurer]].UpdateEnrollee(enrolleeInfo);
+}
+
+void DisplayResults()
+{
+    var csv = new StringBuilder();
+    if(File.Exists(sortedCSV)) File.Delete(sortedCSV);
+    csv.AppendLine("UserID,firstName,lastName,Version,InsuranceCompany");
+    foreach (KeyValuePair<string, InsuranceCompany> ic in GlobalVars.insuranceCompanies.OrderBy(x=>x.Key))
+    {
+        Console.WriteLine(ic.Key);
+        foreach (KeyValuePair<string, Enrollee> en in ic.Value.enrollees
+            .OrderBy(l=>l.Value.lastName).ThenBy(f=>f.Value.firstName))
+        { 
+            var newLine = en.Value.ToString();
+            Console.WriteLine(newLine);
+            csv.AppendLine(newLine);
+        }
+    }
+    File.WriteAllText(sortedCSV, csv.ToString());
+}
+
+static class GlobalVars
+{
+    public static Dictionary<string, InsuranceCompany> insuranceCompanies = new();
+}
